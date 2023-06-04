dae7692c95a72eaacbf7fce9e80c3e2b29768eee
Babalola Precious <93809907+jaypeebabalola@users.noreply.github.com>
2023-06-04 20:23:32 +0100

    Add files via upload

---
+++ website/__init__.py
@@ -0,0 +1,56 @@
+from flask import Flask
+import secrets
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+login_manager = LoginManager()
+mail = Mail()
+
+def create_app():
+    app = Flask(__name__)
+    secret_key = secrets.token_hex(16)
+    app.config['SECRET_KEY'] = secret_key
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    
+    # login_manager.init_app(app)
+    # mail.init_app(app)
+
+    from .views import views 
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User
+
+    with app.app_context():
+        create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = 'precious.babalola@stu.cu.edu.ng'
+    app.config['MAIL_PASSWORD'] = '*************' 
+
+    return app 
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all()
+        print('Created Database!')
